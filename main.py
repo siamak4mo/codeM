1ae348efbd25af46add1dd5b6c3583171aacbf6d
iss <iss.siamak@gmail.com>
2022-05-31 17:07:10 +0430

    add base for creating random code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-from codem import is_valid_v, mk_rand_codem
+from codem import is_valid_v, mk_rand_codem, city_name
 
 
 if __name__ == '__main__':
@@ -11,8 +11,20 @@ if __name__ == '__main__':
         if(comm == '1'):
             print(is_valid_v(input("your code> ")))
         elif(comm == '2'):
-            print(mk_rand_codem())
-        elif(comm.replace(' ','') == ''):
+            base = input("enter a base (city code) or leave it empty> ")
+            if(len(base) > 8):
+                break
+            if(base == ''):
+                base = None
+            else:
+                base = "0"*(3-len(base)) + base
+                cn = city_name(base)
+                if(cn is None):
+                    print("%s is not a valid city code" % base)
+                else:
+                    print("محل صدور: %s" % cn)
+            print(mk_rand_codem(base))
+        elif(comm.replace(' ', '') == ''):
             pass
         else:
             quit = 1
